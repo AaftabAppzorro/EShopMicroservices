71baeacfa2726c4e8e2c7062d4d64795940444cc
AaftabAppzorro <aaftab.appzorro@gmail.com>
2024-07-08 19:49:33 +0530

    Section 18

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,11 +1,14 @@
 ﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(config =>
         {
@@ -14,6 +17,11 @@ public static class DependencyInjection
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
-        return services;
+		//Registering Feature Management Services for Dependency Injection Registration
+		services.AddFeatureManagement();
+
+		//Async communication services RabbitMQ
+		services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+		return services;
     }
 }
